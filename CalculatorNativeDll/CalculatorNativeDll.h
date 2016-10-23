4d8963d1a5055406e0f75231e9e792b341a08cf5
Kenneth Nielsen <kenneth.f.nielsen@gmail.com>
2016-10-23 09:31:54 +0200

    first add of projects and files

---
+++ CalculatorNativeDll/CalculatorNativeDll.h
@@ -0,0 +1,13 @@
+// CalculatorNativeDll.h
+
+#pragma once
+
+using namespace System;
+
+namespace CalculatorNativeDll {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
---
+++ CalculatorTest/UnitTest/UnitTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using CalculatorDll;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTest
+{
+    /// <summary>
+    /// Summary description for UnitTest
+    /// </summary>
+    [TestClass]
+    public class UnitTest
+    {
+        private Class1 class1;
+        public UnitTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            class1 = new Class1();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            class1 = null;
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsInstanceOfType(class1,typeof(Class1));
+            class1.a = 0;
+            Assert.AreEqual(0,class1.a);
+        }
+    }
+}
