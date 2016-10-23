eb106beeb7723bea87c1f711312f63b2661dfc43
Kenneth Nielsen <kenneth.f.nielsen@gmail.com>
2016-10-23 22:56:39 +0200

    Renamed stuff

---
+++ CalculatorDll/Form/CalculatorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDll.Form
+{
+    public class CalculatorController
+    {
+        public CalculatorController()
+        {
+            
+        }
+        public T Add<T>(T first,T second)
+        {
+            return first + second;
+        }
+    }
+}
---
+++ CalculatorDll/Form/CalculatorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorDll.Form
+{
+    public class CalculatorViewModel
+    {
+        public CalculatorViewModel()
+        {
+            
+        }
+
+    }
+}
