9dd7a7969e265cb8c2f8b2016233559fedc5147d
kfn <kenneth.f.nielsen@gmail.com>
2016-10-24 16:18:11 +0200

    Renamed, Cleaned up and added tests

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,23 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using CalculatorDll.Form;
 
 namespace CalculatorApp
 {
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault( false );
-            Application.Run( new CalculatorForm() );
-        }
-    }
+   static class Program
+   {
+      /// <summary>
+      /// The main entry point for the application.
+      /// </summary>
+      [STAThread]
+      static void Main()
+      {
+         Application.EnableVisualStyles();
+         Application.SetCompatibleTextRenderingDefault(false);
+         Application.Run(new CalculatorForm());
+      }
+   }
 }
--- CalculatorDll/Form/CalculatorViewModel.cs
+++ CalculatorDll/Form/CalculatorViewModel.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CalculatorDll.Form
+﻿namespace CalculatorDll.Form
 {
-    public class CalculatorViewModel
-    {
-        public CalculatorViewModel()
-        {
-            
-        }
+   public class CalculatorViewModel
+   {
+      public CalculatorViewModel()
+      {
 
-    }
+      }
+
+   }
 }
